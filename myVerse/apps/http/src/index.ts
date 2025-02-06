2789ded016bc08486fbbb748d5457909aecf9129
Turbobot <turbobot@vercel.com>
2025-02-06 23:24:51 +0530

    feat(create-turbo): apply official-starter transform

--- myVerse/apps/http/src/index.ts
+++ myVerse/apps/http/src/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { router } from "./routes/v1/index.route";
+import client from "@repo/db"
 
 const app = express();
 
 app.use("/api/v1", router);
 
-app.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000); 
 
