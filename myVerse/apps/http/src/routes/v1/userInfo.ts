d6fd6183f970abe988e4937ddd652a41be9e3130
Turbobot <turbobot@vercel.com>
2025-02-07 11:47:00 +0530

    feat(create-turbo): apply official-starter transform

--- myVerse/apps/http/src/routes/v1/userInfo.ts
+++
@@ -1,7 +0,0 @@
-import { Router } from "express";
-import { userInfo } from "../../controllers/v1/userInfo";
-
-export const userInfoRouter = Router();
-
-userInfoRouter.post("/metadata", userInfo[0]);
-userInfoRouter.get("/metadata/bulk", userInfo[1]);
